a681a420b450d8481ce0d264762e5d0c291abc62
Tabolich-Aliaksei <a.tobolitch@yandex.by>
2023-02-21 16:17:08 +0300

    lesson_5 Logout fix

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 
@@ -13,7 +13,7 @@ class CustomLoginView(LoginView):
         }
         messages.add_message(self.request, messages.INFO, mark_safe(message))
         return ret
-
+    
     def form_invalid(self, form):
         for _unused, msg in form.error_messages.items():
             messages.add_message(
@@ -22,3 +22,8 @@ class CustomLoginView(LoginView):
                 mark_safe(f"Something goes worng:<br>{msg}"),
             )
         return self.render_to_response(self.get_context_data(form=form))
+    
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.add_message(self.request, messages.INFO, _("See you later!"))
+        return super().dispatch(request, *args, **kwargs)
